8fca6a40765400eaedb2b47f855af1e0716eef30
Adithiyaa <Adithiyaa@DESKTOP-96LE035>
2025-08-27 20:39:21 +0530

    Assignment 3 completed

---
+++ Assignment3/src/com/Adithiyaa/assignment03/Bookshelf.java
@@ -0,0 +1,24 @@
+package com.Adithiyaa.assignment03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bookshelf {
+	
+	 private List<Product> items;
+
+	    public Bookshelf() {
+	        items = new ArrayList<>();
+	    }
+
+	    public void addItem(Product item) {
+	        items.add(item);
+	    }
+
+	    public void showAllItems() {
+	        for (Product item : items) {
+	            item.displayInfo();
+	            System.out.println("-------------------");
+	        }
+	    }
+}
